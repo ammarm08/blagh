cda1ddb745c6e5781537aedc9d0b5e240b284e17
Ammar <ammar.mian08@gmail.com>
2018-10-16 16:41:13 -0400

    parser: update grammar

--- blagh/parser/__init__.py
+++ blagh/parser/__init__.py
@@ -3,14 +3,14 @@ Parses a blagh file.
 
 Grammar:
 
-program := block*
-block :=
+program := tag*
+tag :=
     "<" + "macros" + ">" + macro_content + "</" + "macros" + ">" ||
     "<" + "globals" + ">" + var_content + "</" + "globals" + ">" ||
     "<" + "variables" + ">" + var_content + "</" + "variables" + ">" ||
     "<" + "imports" + ">" + import_content + "</" + "imports" + ">" ||
-    "<" + custom_block + ">" + custom_content + "</" + custom_block + ">"
-custom_block := STRING
+    "<" + custom_tag + ">" + custom_content + "</" + custom_tag + ">"
+custom_tag := STRING
 
 macro_content := html_template*
 var_content := var_assignment*
@@ -18,7 +18,7 @@ import_content := (var_name + NEWLINE)*
 custom_content := STRING
 
 var_assignment := var + STRING + NEWLINE
-html_template := HTML_OPENING_TAG + injectable || html_template + HTML_CLOSING_TAG
+html_template := HTML_OPENING_TAG + [injectable or html_template] + HTML_CLOSING_TAG
 
 injectable := "{}"
 var := "$" + STRING + "$"
